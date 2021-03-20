2b6c4f00324b55c03b60e8c2f605329aca60f2c0
cardosovictorr <62979369+cardosovictorr@users.noreply.github.com>
2021-03-20 14:55:28 +1100

    navbar starte, still withou CSS style coded

---
+++ src/components/NavBarCustom.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+//import 'bootstrap/dist/css/bootstrap.min.css'
+//import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap'
+
+function NavBarCustom() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to='/' className='navbar-logo'>
+                        StockBot <i className='fab fa-typo3' />
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        {/* to handle the icon that show if in mobile or desktop, we dot the following code */}
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'} >
+                        <li>
+                            <Link
+                                to='/'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to='/aboutUs'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                About Us
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to='/services'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                Services
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to='/'
+                                className='nav-links-mobile'
+                                onClick={closeMobileMenu}
+                            >
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+
+
+        // OLD VERSION BELLOW
+        // <Navbar bg="light" expand="lg">
+        //     <Navbar.Brand href="#home">StockBot</Navbar.Brand>
+        //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        //     <Navbar.Collapse id="basic-navbar-nav">
+        //         <Nav className="mr-auto">
+        //             <Nav.Link href="#home">Home</Nav.Link>
+        //             <Nav.Link href="#About Us">About Us</Nav.Link>
+        //             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        //                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        //                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        //                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        //                 <NavDropdown.Divider />
+        //                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        //             </NavDropdown>
+        //         </Nav>
+        //         {/* <Form inline>
+        //             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        //             <Button variant="outline-success">Search</Button>
+        //         </Form> */}
+        //     </Navbar.Collapse>
+        // </Navbar>
+    );
+}
+
+export default NavBarCustom;

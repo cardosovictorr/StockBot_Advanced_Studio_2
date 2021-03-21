b3c91cb4c3e0740de2fbb9932d53c6eb2a50176d
DarkenedLegacyX <7092@ait.nsw.edu.au>
2021-03-21 22:24:31 +1100

    Change color, add video to homepage

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../App.css';
-import { ButtonCustom } from './ButtonCustom.js'
+import { ButtonCustom } from './ButtonCustom'
 import '../styles/HeroSection.css';
 
 function HeroSection() {
     return (
         <div className='hero-container'>
+            <video src='./videos/test1.mp4' autoPlay loop muted />
             <h1>Stock BOT</h1>
             <p>Automatize your stock order...</p>
             <div className='hero-btns'>
